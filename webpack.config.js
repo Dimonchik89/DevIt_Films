6bdecfedbf7f9ef2bd1cc419e425b415ef356e8f
Dimonchik89 <71697824+Dimonchik89@users.noreply.github.com>
2023-04-06 06:44:40 +0200

    change build directory for public

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "builder"),
+        path: path.resolve(__dirname, "public"),
         filename: "builder.js",
         publicPath: "/"
     },
